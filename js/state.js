f929fb0b4dd548ae57a570086b2da78ba7ceaa94
Lucca Lopes <lopesf.lucca@gmail.com>
2023-06-27 19:43:16 -0300

    adiciona operação mover

--- js/state.js
+++ js/state.js
@@ -3,3 +3,7 @@ let tabuleiro = ['p', 'p', 'p', null, 'b', 'b', 'b'];
 export function getTabuleiro(){
     return structuredClone(tabuleiro);
 }
+
+export function mover(de, para){
+    return false;
+}
